8502accdd95809985fa81c374cfcaa47d8e04d70
Nitish kumar <krnitish540@gmail.com>
2023-12-01 19:11:30 +0530

    Final changes

--- server/index.js
+++ server/index.js
@@ -3,11 +3,14 @@ import mongoose from 'mongoose'
 import  dotenv from 'dotenv'
 dotenv.config()
 import Link from './models/Link.js'
+import path from 'path'
 
 const app=express()
 app.use(express.json())
 
-const PORT= process.env.PORT || 5000
+const __dirname= path.resolve()
+
+
 const MongoDB = async ()=>{
     const connect= await mongoose.connect(process.env.MONGODB_URL)
     if(connect){
@@ -41,9 +44,7 @@ const ramdonSlug=Math.random().toString(36).substring(2,7)
  app.get('/:slug', async (req,res)=>{
     const {slug}=req.params
     const  link = await Link.findOne({slug:slug})
-    await Link.updateOne({slug:slug}, {$set:{
-        click:link.click+1
-    }})
+
     if(!link){
         return res.json({
             success:false,
@@ -51,6 +52,10 @@ const ramdonSlug=Math.random().toString(36).substring(2,7)
         })
     }
 
+    await Link.updateOne({slug:slug}, {$set:{
+        click:link.click+1
+    }})
+    
     res.redirect(link.url)
  })
 
@@ -64,6 +69,16 @@ const ramdonSlug=Math.random().toString(36).substring(2,7)
         });
  })
 
+ if(process.env.NODE_ENV=== "production"){
+    app.use(express.static(path.join(__dirname,'..','client','build')))
+
+    app.get('*', (req,res)=>{
+        res.sendFile(path.join(__dirname,'..','client','build','index.html'))
+    })
+ }
+
+
+const PORT= process.env.PORT || 5000
 app.listen(PORT,()=>{
     console.log(`Server started on port ${PORT}`)
     MongoDB().catch((err)=>{console.error(err)})
